edb2460b62694c710e831a83502869efbb9cc163
Gabriel Veloso <gabrielveloso2@gmail.com>
2020-04-19 21:32:39 -0300

    first commit

---
+++ blog_project/app/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import  User
+from ckeditor.fields import RichTextField
+from ckeditor_uploader.fields import RichTextUploadingField
+
+# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    summary =  RichTextField()
+    content = RichTextUploadingField()
+    author = models.ForeignKey(User, on_delete=models.PROTECT)
+    create_at = models.DateField(auto_now_add=True)
